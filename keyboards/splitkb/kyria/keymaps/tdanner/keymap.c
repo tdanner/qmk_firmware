26d77ee289cce6041bdaa04021aca5a419a7f879
Tim Danner <tim@danners.us>
2023-02-07 15:43:46 -0600

    Space cadet shifts

--- keyboards/splitkb/kyria/keymaps/tdanner/keymap.c
+++ keyboards/splitkb/kyria/keymaps/tdanner/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * |  Bksp  |   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |Ctrl/' "|
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  | [ {  |Hyper\|  |F-keys|  ] } |   N  |   M  | ,  < | . >  | /  ? | RShift |
+ * |(LShift |   Z  |   X  |   C  |   V  |   B  | [ {  |Hyper\|  |F-keys|  ] } |   N  |   M  | ,  < | . >  | /  ? | RShift)|
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        |Adjust| LGUI | LGUI/| Space| Sym  |  | Nav  | LAlt/| AltGr| RGUI | Menu |
  *                        |      |      |  Esc |      |      |  |      | Enter|      |      |      |
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_MINS,
      KC_BSPC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,HYP_BSLS,    FKEYS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
+     SC_LSPO , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,HYP_BSLS,    FKEYS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, SC_RSPC,
                                 ADJUST , KC_LGUI, CMD_ESC, KC_SPC , SYM    ,    NAV    , ALT_ENT,KC_RALT, KC_RGUI, KC_APP
     ),
 
