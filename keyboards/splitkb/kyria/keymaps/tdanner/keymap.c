111ccb27fb1cd08085f0dcf816e99501aa7718f6
Tim Danner <tim@danners.us>
2023-02-08 12:19:07 -0600

    Backtick to top left, tab to right thumb

--- keyboards/splitkb/kyria/keymaps/tdanner/keymap.c
+++ keyboards/splitkb/kyria/keymaps/tdanner/keymap.c
@@ -37,6 +37,7 @@ enum layers {
 #define CTL_QUOT MT(MOD_RCTL, KC_QUOTE)
 #define CTL_MINS MT(MOD_RCTL, KC_MINUS)
 #define ALT_ENT  MT(MOD_LALT, KC_ENT)
+#define ALT_TAB  MT(MOD_LALT, KC_TAB)
 #define HYP_BSLS MT(MOD_HYPR, KC_BSLS)
 
 // Note: LAlt/Enter (ALT_ENT) is not the same thing as the keyboard shortcut Alt+Enter.
@@ -49,21 +50,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Base Layer: QWERTY
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |  Tab   |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |   -    |
+ * |  ` ~   |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |   -    |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * |  Bksp  |   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |Ctrl/' "|
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * |(LShift |   Z  |   X  |   C  |   V  |   B  | [ {  |Hyper\|  |F-keys|  ] } |   N  |   M  | ,  < | . >  | /  ? | RShift)|
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |Adjust| LGUI | LGUI/| Space| Sym  |  | Nav  | LAlt/| AltGr| RGUI | Menu |
- *                        |      |      |  Esc |      |      |  |      | Enter|      |      |      |
+ *                        |Adjust| LGUI | LGUI/| Space| Sym  |  | Nav  | LAlt/| LAlt/| RGUI | Menu |
+ *                        |      |      |  Esc |      |      |  |      | Enter|  Tab |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-     KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_MINS,
+     KC_GRV  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_MINS,
      KC_BSPC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
      SC_LSPO , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,HYP_BSLS,    FKEYS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, SC_RSPC,
-                                ADJUST , KC_LGUI, CMD_ESC, KC_SPC , SYM    ,    NAV    , ALT_ENT,KC_RALT, KC_RGUI, KC_APP
+                                ADJUST , KC_LGUI, CMD_ESC, KC_SPC , SYM    ,    NAV    , ALT_ENT,ALT_TAB, KC_RGUI, KC_APP
     ),
 
 /*
