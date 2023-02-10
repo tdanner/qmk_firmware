a59558fdfa3d5c823731d0adaf0b27d62e0f902e
Tim Danner <tim@danners.us>
2023-02-09 20:21:54 -0600

    \ | to right thumb

--- keyboards/splitkb/kyria/keymaps/tdanner/keymap.c
+++ keyboards/splitkb/kyria/keymaps/tdanner/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * |(LShift |   Z  |   X  |   C  |   V  |   B  | [ {  |Hyper\|  |F-keys|  ] } |   N  |   M  | ,  < | . >  | /  ? | RShift)|
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |Adjust| LGUI | LGUI/| Space| Sym  |  | Nav  | LAlt/| LAlt/| RGUI | Menu |
+ *                        |Adjust| LGUI | LGUI/| Space| Sym  |  | Nav  | LAlt/| LAlt/| RGUI | \ |  |
  *                        |      |      |  Esc |      |      |  |      | Enter|  Tab |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_GRV  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_MINS,
      KC_BSPC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
      SC_LSPO , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,HYP_BSLS,    FKEYS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, SC_RSPC,
-                                ADJUST , KC_LGUI, CMD_ESC, KC_SPC , SYM    ,    NAV    , ALT_ENT,ALT_TAB, KC_RGUI, KC_APP
+                                ADJUST , KC_LGUI, CMD_ESC, KC_SPC , SYM    ,    NAV    , ALT_ENT,ALT_TAB, KC_RGUI, KC_BSLS
     ),
 
 /*
